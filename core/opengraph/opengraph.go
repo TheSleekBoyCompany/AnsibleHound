5137612e32decfda43c2069078c5e8c6c52e462a
Ramoreik <charl-alexandre@ramoreik.com>
2026-01-18 14:32:57 -0500

    - Removed now unused code

--- core/opengraph/opengraph.go
+++ core/opengraph/opengraph.go
@@ -7,31 +7,6 @@ import (
 
 const SOURCE_KIND = "AnsibleBase"
 
-type OutputJson struct {
-	Metadata Metadata `json:"metadata"`
-	Graph    Graph    `json:"graph"`
-}
-
-type Metadata struct {
-	SourceKind string `json:"source_kind,omitempty"`
-}
-
-type Graph struct {
-	Nodes []*node.Node `json:"nodes"`
-	Edges []Edge       `json:"edges"`
-}
-
-type Edge struct {
-	Kind  string       `json:"kind"`
-	Start StartEndNode `json:"start"`
-	End   StartEndNode `json:"end"`
-}
-
-type StartEndNode struct {
-	Value string `json:"value"`
-	Kind  string `json:"kind,omitempty"`
-}
-
 func InitGraph() (graph gopengraph.OpenGraph) {
 	graph = *gopengraph.NewOpenGraph(SOURCE_KIND)
 	return graph
