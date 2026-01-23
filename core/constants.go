321485ff5b489eb26e8e5b5213dbac0edc04556e
Ramoreik <charl-alexandre@ramoreik.com>
2026-01-22 19:39:25 -0500

    + Moved gathering logic to gather package, moved linking logic to opengraph package

--- core/constants.go
+++ core/constants.go
@@ -1,31 +1,5 @@
 package core
 
-import "fmt"
-
 const VERSION = "0.0.1"
 
-const API_ENDPOINT = "/api/v2/"
-const ME_ENDPOINT = API_ENDPOINT + "me/"
-const ORGANIZATIONS_ENDPOINT = API_ENDPOINT + "organizations/"
-const PROJECTS_ENDPOINT = API_ENDPOINT + "projects/"
-const INVENTORIES_ENDPOINT = API_ENDPOINT + "inventories/"
-const JOB_TEMPLATE_ENDPOINT = API_ENDPOINT + "job_templates/"
-const CREDENTIALS_ENDPOINT = API_ENDPOINT + "credentials/"
-const CREDENTIAL_TYPES_ENDPOINT = API_ENDPOINT + "credential_types/"
-const USERS_ENDPOINT = API_ENDPOINT + "users/"
-const USER_ROLES_ENDPOINT = API_ENDPOINT + "users/%d/roles/"
-const GROUPS_ENDPOINT = API_ENDPOINT + "groups/"
-const GROUP_HOSTS_ENDPOINT = API_ENDPOINT + "groups/%d/hosts/"
-const JOBS_ENDPOINT = API_ENDPOINT + "jobs/"
-const HOSTS_ENDPOINT = API_ENDPOINT + "hosts/"
-const TEAMS_ENDPOINT = API_ENDPOINT + "teams/"
-const TEAM_ROLES_ENDPOINT = API_ENDPOINT + "teams/%d/roles/"
-const TEAM_USERS_ENDPOINT = API_ENDPOINT + "teams/%d/users/"
-const JOB_TEMPLATE_CREDENTIALS_ENDPOINT = API_ENDPOINT + "job_templates/%d/credentials/"
-const PING_ENDPOINT = API_ENDPOINT + "ping"
-const PAGE_SIZE = 200
-
 const LDAP_VALUE = "ldap"
-
-var PAGE_SIZE_ARG = fmt.Sprintf("?page_size=%d", PAGE_SIZE)
-var CURRENT_PAGE_ARG = "&page=%d"
--- core/gather/client.go
+++ core/gather/client.go
@@ -1,13 +1,126 @@
-package core
+package gather
 
 import (
 	"ansible-hound/core/ansible"
+	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+
+	"github.com/charmbracelet/log"
 )
+
+type AHClient struct {
+	Client  *http.Client
+	Headers http.Header
+}
+
+func (ahc *AHClient) Do(req *http.Request) (*http.Response, error) {
+	for k, vals := range ahc.Headers {
+		for _, v := range vals {
+			req.Header.Add(k, v)
+		}
+	}
+	return ahc.Client.Do(req)
+}
+
+func (ahc *AHClient) ExecuteReq(req *http.Request) ([]byte, error) {
+
+	resp, err := ahc.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		return []byte{}, fmt.Errorf("HTTP error occurred: %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return body, nil
+}
+
+func (ahc *AHClient) GetPage(url string, currentPage int) ([]byte, error) {
+
+	req, err := initReq(url, currentPage)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	body, err := ahc.ExecuteReq(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return body, nil
+}
+
+func InitClient(proxyURL *url.URL, skipVerifySSL bool,
+	username string, password string, token string) AHClient {
+	// Returns a client configured for the gatherer.
+	// For now, this only configures the Proxy, might be useful to handle SSL verification too.
+
+	transport := &http.Transport{}
+
+	if skipVerifySSL {
+		TLSClientConfig := &tls.Config{
+			InsecureSkipVerify: true,
+		}
+		transport.TLSClientConfig = TLSClientConfig
+	}
+
+	if proxyURL != nil {
+		transport.Proxy = http.ProxyURL(proxyURL)
+	}
+
+	httpClient := &http.Client{
+		Transport: transport,
+	}
+
+	headers := http.Header{}
+	if username != "" && password != "" {
+		raw := username + ":" + password
+		encoded := base64.StdEncoding.EncodeToString([]byte(raw))
+		headers.Add("Authorization", "Basic "+encoded)
+		if token != "" {
+			log.Warn("Prioritizing username/password authentication material, token was ignored.")
+		}
+	} else if token != "" {
+		if password != "" || username != "" {
+			log.Warn("Prioritizing token authentication material, username/password was ignored.")
+		}
+		headers.Add("Authorization", "Bearer "+token)
+	} else {
+		log.Fatal("No authentication material provided, exiting.")
+	}
+
+	client := AHClient{
+		Client:  httpClient,
+		Headers: headers,
+	}
+
+	return client
+}
+
+func initReq(url string, currentPage int) (*http.Request, error) {
+
+	url = url + PAGE_SIZE_ARG + fmt.Sprintf(CURRENT_PAGE_ARG, currentPage)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+
+}
 
 func Gather[T ansible.AnsibleType](client AHClient, target url.URL,
 	endpoint string) ([]T, error) {
@@ -19,7 +132,7 @@ func Gather[T ansible.AnsibleType](client AHClient, target url.URL,
 
 	url := target.String() + endpoint
 
-	body, err := client.getPage(url, page)
+	body, err := client.GetPage(url, page)
 	if err != nil {
 		return nil, err
 	}
@@ -38,7 +151,7 @@ func Gather[T ansible.AnsibleType](client AHClient, target url.URL,
 	if count >= PAGE_SIZE {
 		for {
 			page += 1
-			body, err := client.getPage(url, page)
+			body, err := client.GetPage(url, page)
 			if err != nil {
 				return nil, err
 			}
