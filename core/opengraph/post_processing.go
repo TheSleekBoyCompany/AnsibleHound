57417ddcbfaca0bea0ec5aec4cfcf8234d6df7e3
Ramoreik <charl-alexandre@ramoreik.com>
2026-02-26 21:43:19 -0500

    + ajout des conditions specifiques pour l'exploitation des edges

--- core/opengraph/post_processing.go
+++ core/opengraph/post_processing.go
@@ -1,6 +1,8 @@
 package opengraph
 
 import (
+	"slices"
+
 	"github.com/Ramoreik/gopengraph"
 )
 
@@ -27,16 +29,58 @@ func PostProcessingCredentials(graph *gopengraph.OpenGraph) {
 
 			if credentialType == "Machine" {
 				machineCredentialType := node.GetProperty("machine_credential_type").(string)
+				identityNode := graph.GetNode(edge.GetStartNodeID())
+				identityEdges := graph.GetEdgesFromNode(identityNode.GetID())
 
 				if machineCredentialType == "ssh" && (edge.GetKind() == "ATUse" || edge.GetKind() == "ATAdmin") {
-					edge = GenerateEdge("ATSSHHijackAgent", edge.GetStartNodeID(), node.GetID())
-					graph.AddEdge(edge)
+					// NEEDED: Control over the ansible playbook, to execute arbitrary commands on the execution environment with `delegate_to`
+					var ok bool
+					for _, edge := range identityEdges {
+						if slices.Contains(graph.GetNode(edge.GetEndNodeID()).GetKinds(), "ATJobTemplate") {
+							// If Admin of a Job Template you can potentially exploit an injection in the playbook.
+							if edge.GetKind() == "ATAdmin" {
+								ok = true
+							}
+						}
+						if slices.Contains(graph.GetNode(edge.GetEndNodeID()).GetKinds(), "ATOrganization") {
+							if edge.GetKind() == "ATAdmin" || edge.GetKind() == "ATJobTemplateAdmin" || edge.GetKind() == "ATProjectAdmin" {
+								ok = true
+							}
+						}
+						if slices.Contains(graph.GetNode(edge.GetEndNodeID()).GetKinds(), "ATProject") {
+							// If ATAdmin of a project you can change the repo and configure a public one.
+							if edge.GetKind() == "ATAdmin" {
+								ok = true
+							}
+						}
+					}
+					if ok {
+						edge = GenerateEdge("ATSSHHijackAgent", edge.GetStartNodeID(), node.GetID())
+						graph.AddEdge(edge)
+					}
 				}
 
 				if machineCredentialType == "password" && (edge.GetKind() == "ATUse" || edge.GetKind() == "ATAdmin") {
-					edge = GenerateEdge("ATCompromiseWithHoneypot", edge.GetStartNodeID(), node.GetID())
-					graph.AddEdge(edge)
+					// NEEDED: user able to modify any inventory, in order to target an attacker controlled server.
+					var ok bool
+					for _, edge := range identityEdges {
+						if slices.Contains(graph.GetNode(edge.GetEndNodeID()).GetKinds(), "ATInventory") {
+							if edge.GetKind() == "ATAdmin" {
+								ok = true
+							}
+						}
+						if slices.Contains(graph.GetNode(edge.GetEndNodeID()).GetKinds(), "ATOrganization") {
+							if edge.GetKind() == "ATInventoryAdmin" || edge.GetKind() == "ATAdmin" {
+								ok = true
+							}
+						}
+					}
+					if ok {
+						edge = GenerateEdge("ATCompromiseWithHoneypot", edge.GetStartNodeID(), node.GetID())
+						graph.AddEdge(edge)
+					}
 				}
+
 			}
 		}
 	}
